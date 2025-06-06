55051a439abaaf9a260138bc90aacc31c9cd8761
Camila Juarez <camilajuarez17@gmail.com>
2025-06-06 19:42:28 -0300

    formulario funcional

--- mi-portfolio-camila/src/componnets/MainContacts.jsx
+++ mi-portfolio-camila/src/componnets/MainContacts.jsx
@@ -1,29 +1,45 @@
 import React from 'react'
+import { useRef } from 'react';
 import { Form , Row, Col , Button} from 'react-bootstrap'
+import emailjs from '@emailjs/browser';
+import "../css/formulario.css"
 
 const MainContacts = () => {
-  return (
-    <div>
-      <Form class="w-50 p-6 display: flex;">
-      <Form.Group className="mb-3" controlId="forEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formAsunto">
-        <Form.Label>Asunto</Form.Label>
-        <Form.Control type="text" placeholder="Asunto" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formMensaje">
-        <Form.Label>Mensaje</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
 
-        <Form.Group as={Row} className="mb-5">
-        <Col sm={{ span: 10, offset: 2 }}>
-          <Button type="submit">Send</Button>
-        </Col>
-      </Form.Group>
-    </Form>
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_us5oxmx', 'template_jargdh8', form.current, {
+        publicKey: 'vbx0tc5p1_8CUSD0b',
+      })
+      .then(
+        () => {
+          alert("Correo enviado con exito !");
+          form.current.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+
+
+  return (
+    <div className='form-container'>
+      <h2 className='title' >Contactame</h2>
+      <form ref={form} onSubmit={sendEmail}>
+      <label>ASUNTO</label>
+      <input type="text" name="user_name" />
+      <label>EMAIL</label>
+      <input type="email" name="user_email" />
+      <label>MENSAJE</label>
+      <textarea name="message" />
+      <input type="submit"  className='submit-button'  value="Send" />
+    </form>
     </div>
   )
 }
